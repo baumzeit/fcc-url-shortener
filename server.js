e88f57d61f533b340bb46b049643cd4569123021
Glitch (spotless-viola) <none>
2018-11-05 22:01:26 +0000

    🍐🏐 Checkpoint ./server.js:678538/130

--- server.js
+++ server.js
@@ -28,8 +28,9 @@ app.get('/', function(req, res){
 
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.post("/api/shorturl/new", function (req, res) {
+  const url = req.body.url;
+  res.json({original_url: url});
 });
 
 
