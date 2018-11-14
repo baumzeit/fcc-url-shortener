ac793b13ffa6fa6e4d5fedb2f06c8e117b0b2ba9
Glitch (spotless-viola) <none>
2018-11-14 18:24:33 +0000

    ⛩🚪 Checkpoint ./server.js:678538/108

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
 var bodyParser = require('body-parser');
 var url = require('url');
 var dns = require('dns');
@@ -16,7 +18,8 @@ var port = process.env.PORT || 3000;
 process.env.MONGOLAB_URI = "mongodb://user:pass-0@ds153093.mlab.com:53093/url-shortener"
 
 /** this project needs a db !! **/ 
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true, useCreateIndex: true });
+
 
 app.use(cors());
 
@@ -46,7 +49,7 @@ app.post("/api/shorturl/new", function (req, res) {
 });
 
 function validateUrlFormat (testString) {
-// checks if the input string follows this format: http(s)://www.example.com(/more/routes)
+// checks if the input string follows this format: http(s)://www.example(.foobar).com(/more/routes)
   return new Promise(function(resolve, reject) {
     const reURL = /https?:\/\/www(\.[0-9a-z$\-_+!*‘(),]{1,}){2,}((\/[0-9a-z$\–_+!*‘(),]{1,})+)?/i;
     if (reURL.test(testString)) {
